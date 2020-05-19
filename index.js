d85ec7e6dbdc09750b647788143dd836e37a62b1
Peter Anderson <pete.anderson@gmail.com>
2020-05-19 20:46:04 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const app = require("express")();
+const server = require("http").Server(app);
+const io = require("socket.io")(server,  { origins: 'https://jolly-khorana-77c4aa.netlify.app:*' });
+
+server.listen(process.env.PORT || 8080, () => console.log("battlemasters socket server up and running!"));
+
+io.on("connection", (socket) => {
+    socket.emit("news", { hello: "world" });
+    socket.on("my other event", (data) => {
+        console.log(data);
+    });
+});
