8ae92772637606aeacc2ffdc9e94aff55a02b1f1
Peter Anderson <pete.anderson@gmail.com>
2020-05-19 20:49:33 +0200

    hooked up heroku

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const io = require("socket.io")(server,  { origins: 'https://jolly-khorana-77c4a
 
 server.listen(process.env.PORT || 8080, () => console.log("battlemasters socket server up and running!"));
 
+app.get('/', (req, res) => {
+    res.json({success: true});
+});
+
 io.on("connection", (socket) => {
     socket.emit("news", { hello: "world" });
     socket.on("my other event", (data) => {
