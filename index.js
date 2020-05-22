2d8ca22a803693d87a03ebe4318dc1acbdfb9282
Peter Anderson <pete.anderson@gmail.com>
2020-05-22 08:47:23 +0200

    more socket setup

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require("express")();
 const server = require("http").Server(app);
-const io = require("socket.io")(server,  { origins: 'https://jolly-khorana-77c4aa.netlify.app:*' });
+const io = require("socket.io")(server,  { origins: 'localhost:3000 https://jolly-khorana-77c4aa.netlify.app:*' });
 
 server.listen(process.env.PORT || 8080, () => console.log("battlemasters socket server up and running!"));
 
@@ -8,10 +8,36 @@ app.get('/', (req, res) => {
     res.json({success: true});
 });
 
+let rooms = {};
+
 io.on("connection", (socket) => {
-    console.log('socket connection!!!!: ',socket);
+    console.log('socket connection!!!!: ',socket.id);
     socket.emit("news", { hello: "world" });
-    socket.on("my other event", (data) => {
-        console.log(data);
+
+    socket.on("check if room exists", ({roomId}) => {
+        console.log("check if room exists: ", roomId);
+        console.log(rooms[roomId]);
+        if(rooms[roomId]) {
+            rooms[roomId].player2 = {
+                socketIds: [socket.id]
+            };
+        }
+        socket.emit("room", rooms);
+    });
+
+    socket.on("new room", ({roomId}) => {
+        console.log(roomId);
+        if (!rooms.roomId) {
+            rooms[roomId] = {
+                'player1': {
+                    socketIds: [socket.id]
+                }
+            };
+        }
+        console.log("rooms: ", rooms);
+        socket.emit("room", rooms);
+    });
+    socket.on("disconnect", () => {
+        console.log("disconnect!!!!", socket.id);
     });
 });
